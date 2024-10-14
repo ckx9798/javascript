7f18ccdacdf83833e7394b235773c50f856c9199
ckx9798 <ckx12@Naver.com>
2024-10-14 23:14:18 +0900

    Feat hidden css

--- chrome-app/app.js
+++ chrome-app/app.js
@@ -1,15 +1,42 @@
+// const loginButton = loginfoam.querySelector("button");
+
+// const loginInput1 = loginfoam.querySelector("#login_foam input");
+// const loginButton2 = loginfoam.querySelector("#login_foam button");
+
+// function handleBtnClick() {
+//   const userName = loginInput.value;
+//   console.log(userName);
+//   if (userName === "") {
+//     alert("Please write your name");
+//   } else if (userName.length > 15) {
+//     alert("write less than 15");
+//   }
+// }
+
+// loginButton.addEventListener("click", handleBtnClick);
+
+/*
+const link = document.querySelector("a");
+
+function handleLinkClick(e) {
+  e.preventDefault();
+  console.log(e);
+  console.dir(e);
+}
+link.addEventListener("click", handleLinkClick);
+*/
+
 const loginfoam = document.querySelector("#login_foam");
 const loginInput = loginfoam.querySelector("input");
-const loginButton = loginfoam.querySelector("button");
+const greeting = document.querySelector("#hidden_h1");
+const HIDDEN_CLASSNAME = "hidden";
 
-function handleBtnClick() {
+function onLoginSubmit(e) {
+  e.preventDefault();
+  loginfoam.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
-  console.log(userName);
-  if (userName === "") {
-    alert("Please write your name");
-  } else if (userName.length > 15) {
-    alert("write less than 15");
-  }
+  greeting.innerText = `Hello ${userName}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", handleBtnClick);
+loginfoam.addEventListener("submit", onLoginSubmit);
