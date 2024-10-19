5de6c8be9ab3cf134c52d6e386bfa99f91042b39
ckx9798 <ckx12@Naver.com>
2024-10-19 23:20:54 +0900

    Feat 버튼-삭제기능 추가

--- chrome-app/js/todo.js
+++ chrome-app/js/todo.js
@@ -1,12 +1,37 @@
-const todoFrom = document.querySelector("#todo_form");
-const todoInput = document.querySelector("#todo_form input");
-const todoList = document.querySelector("#todo_list");
+const toDoForm = document.getElementById("todo_form");
+const toDoInput = document.querySelector("#todo_form input");
+const toDoList = document.getElementById("todo_list");
 
-function handleTodoSubmit(e) {
-  e.preventDefault;
-  const newTodo = todoInput.value;
-  todoInput.value = "";
-  console.log(newTodo);
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
 }
 
-todoFrom.addEventListener("submit", handleTodoSubmit);
+function printTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+const todoArr = [];
+
+function saveTodo() {
+  localStorage.setItem("todos", JSON.stringify(todoArr));
+}
+
+function handleTodoSubmit(e) {
+  e.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  todoArr.push(newTodo);
+  printTodo(newTodo);
+  saveTodo();
+}
+
+toDoForm.addEventListener("submit", handleTodoSubmit);
