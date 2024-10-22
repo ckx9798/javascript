0b9fd908cce4a0d97f088e23e52cd783a5da9a7c
ckx9798 <ckx12@Naver.com>
2024-10-22 18:55:20 +0900

    📕 코딩테스트 1번

--- js/00_dailiy_coding/01_transformFirstAndLast.js
+++ js/00_dailiy_coding/01_transformFirstAndLast.js
@@ -1,4 +1,3 @@
-/*
 문제
 배열을 입력받아 차례대로 배열의 첫 요소와 마지막 요소를 키와 값으로 하는 객체를 리턴해야 합니다.
 
@@ -13,21 +12,30 @@ string 타입을 요소로 갖는 배열
 빈 배열을 입력받은 경우, 빈 객체를 리턴해야 합니다.
 입력으로 받는 배열을 수정하지 않아야 합니다.
 
-입출력 예시
-let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
-let output = transformFirstAndLast(arr);
-console.log(output); // --> { Queen : 'Beyonce' }
-arr = ['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
-output = transformFirstAndLast(arr);
-console.log(output); // --> { Kevin : 'Spacey' }
-*/
+// 입출력 예시
+// let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
+// let output = transformFirstAndLast(arr);
+// console.log(output); // --> { Queen : 'Beyonce' }
+// arr = ['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
+// output = transformFirstAndLast(arr);
+// console.log(output); // --> { Kevin : 'Spacey' }
 
-function transformFirstAndLast(arr) {
-  // TODO: 여기에 코드를 작성합니다.
-  let obj={}
-  if(arr.length===0) return {}
-  let first = arr[0]
-  let last = arr[arr.length-1]
-  obj[first] = last
-  return obj
+// function transformFirstAndLast(arr) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let obj={}
+//   if(arr.length===0) return {}
+//   let first = arr[0]
+//   let last = arr[arr.length-1]
+//   obj[first] = last
+//   return obj
+// }
+
+function makeNewObj(arr) {
+  let answer = {}
+  const firstA = arr[0]
+  const lastA = arr[arr.length-1]
+  answer[firstA] = lastA
+  return answer
 }
+
+console.log(makeNewObj(["qwe","asd","1q"]))
