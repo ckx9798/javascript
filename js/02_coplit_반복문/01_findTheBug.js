9eb30e8560462e463f399073e40e0020d4cf1f00
MJ <ckx9305@gmail.com>
2024-11-08 22:01:24 +0900

    📙 코딩테스트 배열-1

--- js/02_coplit_반복문/01_findTheBug.js
+++ js/02_coplit_반복문/01_findTheBug.js
@@ -10,8 +10,8 @@ string 타입의 단어
 number 타입을 리턴해야 합니다.
 
 주의 사항
-반복문(for)문을 사용해야 합니다.
 '#' 기호가 없는 경우 undefined를 리턴해야 합니다.
+반복문(for)문을 사용해야 합니다.
 
 입출력 예시
 let output = findTheBug('wo#rd');
@@ -24,10 +24,22 @@ output = findTheBug('bug');
 console.log(output); // --> undefined
 */
 
-function findTheBug(word) {
-  // TODO: 여기에 코드를 작성합니다.
-  for(let i=0; i<word.length; i++){
-    if(word[i]==='#') return i;
+// function findTheBug(word) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   for(let i=0; i<word.length; i++){
+//     if(word[i]==='#') return i;
+//   }
+//   return undefined;
+// }
+
+function findThebug(word) {
+  const answer = word.split("");
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === "#") {
+      return i;
+    }
   }
   return undefined;
 }
+
+console.log(findThebug("wor#d"));
